46ca291094a99a0ef6fe6c8dc18261497aa233e8
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-10 08:24:44 +0530

    Add: Print The Prime Factors Of number N

---
+++ PrimeFactors.java
@@ -0,0 +1,23 @@
+package com.assignments.day5.PracticeProblem;
+
+import java.util.Scanner;
+
+public class PrimeFactors {
+	   public static void main(String args[]){
+		      int number;
+		      Scanner sc = new Scanner(System.in);
+		      System.out.println("Enter a number to check its Prime Factors ::");
+		      number = sc.nextInt();
+		      System.out.println("Prime Factors ::");
+		      for(int i = 2; i< number; i++) {
+		         while(number%i == 0) {
+		            System.out.println(i+" ");
+		            number = number/i;
+		         }
+		      }
+		      if(number >2) {
+		         System.out.println(number);
+		      }
+		   }
+
+}
